64826526d5c15454450373503b8b9cf62dcff9d2
Oleksandr Solovei <solovei.oleksandr@gmail.com>
2020-03-09 00:25:47 +0200

    4th commit. added comments

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ console.log(2 || 3); //2
 console.log(2 && 3); //3
 console.log(typeof null); // object, it is very strange! but true
 console.log(typeof undefined); // undefined
+// some comments for 4th commit
---
+++ script_new.js
@@ -0,0 +1,1 @@
+//empty file for 4th commit

3ef18e1614bbf0bc0a4230b3d939d5c8a2755370
Oleksandr Solovei <solovei.oleksandr@gmail.com>
2020-03-08 23:39:10 +0200

    third commit

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log(2 || 3);
+console.log(2 || 3); //2
+console.log(2 && 3); //3
+console.log(typeof null); // object, it is very strange! but true
+console.log(typeof undefined); // undefined

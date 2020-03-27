55836a85c6741a186f541c47500bed59cf972245
Oleksandr Solovei <solovei.oleksandr@gmail.com>
2020-03-27 18:06:38 +0200

    json ajax

---
+++ 35_json.js
@@ -0,0 +1,18 @@
+// let options = {
+//     width: 1366,
+//     height: 768,
+//     background: 'red',
+//     font: {
+//         size: '16px',
+//         color: '#fff'
+//     }
+// };
+
+// console.log(JSON.stringify(options)); // evrth in "..."
+// console.log(JSON.parse(JSON.stringify(options)));
+
+// AJAX (/ˈeɪdʒæks/; short for "Asynchronous JavaScript and XML")
+
+// ======================================================
+
+
